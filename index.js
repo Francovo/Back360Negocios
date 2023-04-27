768f376a02fa7bec4bcadbc7a0c96e4d3819b608
Franco <francovelasco10@hotmail.com>
2023-04-27 09:23:08 -0500

    actualizacion comandos

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-const app =  require('express')()
-const http = require('http').Server(app)
-const io = require('socket.io')(http, {
-    cors: {
-        origin: true,
-        Credential: true,
-        methods: ['GET', 'POST']
-    }
-})
+// const app =  require('express')()
+// const http = require('http').Server(app)
+// const io = require('socket.io')(http, {
+//     cors: {
+//         origin: true,
+//         Credential: true,
+//         methods: ['GET', 'POST']
+//     }
+// })
 
 
-io.on("connection",(socket) => {
-     console.log("Nuevo Usuario Conectado");
+// io.on("connection",(socket) => {
+//      console.log("Nuevo Usuario Conectado");
 
-     socket.on("sendMessage", (messageInfo) => {
-        console.log("Enviando un mensaje");
-        socket.broadcast.emit("receiveMessage", messageInfo)
-     })
-})
+//      socket.on("sendMessage", (messageInfo) => {
+//         console.log("Enviando un mensaje");
+//         socket.broadcast.emit("receiveMessage", messageInfo)
+//      })
+// })
 
 
-app.get('/', (req, res) => {
-    res.send("<h1>intento de chat</h1>")
-})
+// app.get('/', (req, res) => {
+//     res.send("<h1>intento de chat</h1>")
+// })
 
-http.listen(3500,() => {
-    console.log('Escuchando en puerto 3500');
-})
+// http.listen(3500,() => {
+//     console.log('Escuchando en puerto 3500');
+// })
+
+
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const port = 3000
+
+
+server.use(middlewares)
+server.use(router)
+
+server.listen(port)
